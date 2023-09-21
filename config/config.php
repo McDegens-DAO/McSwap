44e64337a3088bbdf01f5160048e99dccde93f25
McDegens <137731698+McDegens@users.noreply.github.com>
2023-09-21 02:34:56 -0400

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 $key = "YOUR_HELIUS_KEY";
 $path = "https://rpc.helius.xyz/?api-key=".$key;
-$whitelist = array("https://your-domain-name.com");
+$whitelist = array("https://your-domain.com");
