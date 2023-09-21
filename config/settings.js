c535310863e6ae7c69305f9911c3230ce8b0de33
McDegens <137731698+McDegens@users.noreply.github.com>
2023-09-21 02:21:46 -0400

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,7 @@
 // config
 let conf = {};
 conf.host = window.location.protocol+"//"+window.location.host; // host domain
+// only edit below this line
 conf.cluster = conf.host + "/rpc/"; // proxy folder (recommended) or full endpoint address
 conf.nft_explorer = "https://solscan.io/token/"; // set path to your preferred nft explorer 
 conf.cnft_explorer = "https://xray.helius.xyz/token/"; // set path to your preferred cnft explorer 
