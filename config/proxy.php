81877734169365afd820f3dd7be9c3dce0e9d36f
McDegens <137731698+McDegens@users.noreply.github.com>
2024-03-15 22:46:14 -0400

    Update proxy.php
    Added CMC details.

--- config/proxy.php
+++ config/proxy.php
@@ -2,3 +2,5 @@
 $key = "YOUR-HELIUS-KEY-HERE";
 $path = "https://rpc.helius.xyz/?api-key=".$key;
 $whitelist = array("https://your-domain.com");
+$cmc_key = "YOUR-CMC-KEY-HERE";
+$cmc_path = "https://pro-api.coinmarketcap.com";
