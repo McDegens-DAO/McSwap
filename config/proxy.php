2d04bb204d65668d80ffa342488da838d2f67165
SolDapper <soldapper.com@gmail.com>
2024-07-23 19:14:23 -0400

    modified:   config/proxy.php

--- config/proxy.php
+++ config/proxy.php
@@ -1,11 +1,9 @@
 <?php
-// regular rpc
-// $key = "f6bcb13f-4855-4733-8308-175911dd9f0c";
-// $path = "https://rpc.helius.xyz/?api-key=".$key;
-// business rpc
+// rpc
 $key = "";
 $path = "https://mainnet.helius-rpc.com/?api-key=".$key;
-
+// cmc
 $cmc_key = "";
 $cmc_path = "https://pro-api.coinmarketcap.com";
+// wl
 $whitelist = array("https://mcswap.xyz");
