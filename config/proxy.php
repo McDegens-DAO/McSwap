dad4d42612c495e790889e8a915d0ff1cb3a9432
SolDapper <soldapper.com@gmail.com>
2024-07-23 21:47:56 -0400

    modified:   .gitignore

--- config/proxy.php
+++ config/proxy.php
@@ -1,9 +1,9 @@
 <?php
 // rpc
-$key = "";
+$key = "0e968359-ff35-4a7c-8f5d-aeca0723089b";
 $path = "https://mainnet.helius-rpc.com/?api-key=".$key;
 // cmc
-$cmc_key = "";
+$cmc_key = "604b948a-7228-4c2d-b8f9-7903a0c18dda";
 $cmc_path = "https://pro-api.coinmarketcap.com";
 // wl
 $whitelist = array("https://mcswap.xyz");
